3df62e7056de17dfbf35b91d477e2f6032b3eb66
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-14 09:53:19 +0600

    working circular progress bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { Drawer } from "expo-router/drawer";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +19,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -26,12 +32,40 @@ export default function RootLayout() {
     return null;
   }
 
+  // return (
+  //   <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+  //     <Stack>
+  //       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+  //       <Stack.Screen name="+not-found" />
+  //     </Stack>
+  //   </ThemeProvider>
+  // );
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="timer" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Home",
+            title: "overview",
+          }}
+        />
+        <Drawer.Screen
+          name="history" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "User",
+            title: "overview",
+          }}
+        />
+        <Drawer.Screen
+          name="settings" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "User",
+            title: "overview",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
