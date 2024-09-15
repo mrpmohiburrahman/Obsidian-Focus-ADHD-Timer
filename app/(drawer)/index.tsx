9600ab79b14077eb3a3a9ec888cc99942d06f6d1
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-15 15:46:16 +0600

    🍱 add viscount image

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -84,7 +84,7 @@ const Index: React.FC<IndexProps> = () => {
     <SafeAreaView style={styles.container}>
       <Image
         style={styles.backgroundImage}
-        source={require("@/assets/images/baron.png")}
+        source={require("@/assets/images/viscount.png")}
         placeholder={{ blurhash }}
         contentFit="cover"
         transition={1000}
