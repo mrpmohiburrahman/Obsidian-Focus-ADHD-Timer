505594bab94d72827fd17e54ed68e244ab8dce8a
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-15 16:35:13 +0600

    🍱 add image for emperor

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -84,7 +84,7 @@ const Index: React.FC<IndexProps> = () => {
     <SafeAreaView style={styles.container}>
       <Image
         style={styles.backgroundImage}
-        source={require("@/assets/images/king.png")}
+        source={require("@/assets/images/emperor.png")}
         placeholder={{ blurhash }}
         contentFit="cover"
         transition={1000}
