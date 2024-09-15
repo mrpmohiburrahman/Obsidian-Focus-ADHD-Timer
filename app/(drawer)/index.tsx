cb1e73d71c3bc0b71314d18cc5778999e9c6de90
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-15 15:52:01 +0600

    🍱 add earl

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -84,7 +84,7 @@ const Index: React.FC<IndexProps> = () => {
     <SafeAreaView style={styles.container}>
       <Image
         style={styles.backgroundImage}
-        source={require("@/assets/images/viscount.png")}
+        source={require("@/assets/images/earl.png")}
         placeholder={{ blurhash }}
         contentFit="cover"
         transition={1000}
