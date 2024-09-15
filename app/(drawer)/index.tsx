32c4f92c20fc73608150dddd7233f086d63fa3e7
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-09-15 16:17:25 +0600

    🍱 add image for prince

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -84,7 +84,7 @@ const Index: React.FC<IndexProps> = () => {
     <SafeAreaView style={styles.container}>
       <Image
         style={styles.backgroundImage}
-        source={require("@/assets/images/duke.png")}
+        source={require("@/assets/images/prince.png")}
         placeholder={{ blurhash }}
         contentFit="cover"
         transition={1000}
